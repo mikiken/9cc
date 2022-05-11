90dfa0a4fe654ecbb818af27b2b56b07d683e218
Kentaro Miki <mikiken.dev@gmail.com>
2022-05-11 15:03:00 +0900

    足し算と引き算を追加

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ assert() {
   expected="$1"
   input="$2"
 
-  ./9cc "$input" > tmp.s
+  ./9cc "$input" > tmp.s #9ccを実行し、アセンブリ(.s)を出力
   cc -o tmp tmp.s
   ./tmp
   actual="$?"
@@ -18,5 +18,6 @@ assert() {
 
 assert 0 0
 assert 42 42
+assert 21 "5+20-4"
 
 echo OK
