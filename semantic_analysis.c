37df2703cdc09a6d855839cc3e041212a8ece244
Kentaro Miki <mikiken.dev@gmail.com>
2022-09-29 00:42:14 +0900

    Refactoring

--- semantic_analysis.c
+++ semantic_analysis.c
@@ -8,7 +8,8 @@ FuncDeclaration *find_declaration_by_name(char *name) {
     if (!declaration->name) {
       error("declaration->nameがNULLポインタです");
     }
-    if (!strcmp(declaration->name, name)) { //名前が一致している場合
+    //名前が一致している場合
+    if (!strcmp(declaration->name, name)) {
       return declaration;
     }
   }
@@ -47,10 +48,21 @@ Node *new_typed_binary(Node *node_typed, Node *lhs_typed, Node *rhs_typed) {
   return node_typed;
 }
 
+// ポインタ演算の調整用に型のサイズに合わせた整数nodeを返す
+Node *new_size_node(TypeKind kind) {
+  Node *size = new_typed_node(new_type(TYPE_INT), new_node(ND_NUM));
+  if (kind == TYPE_INT) {
+    size->val = SIZE_INT;
+  }
+  else {
+    size->val = SIZE_PTR;
+  }
+  return size;
+}
+
 Node *cast_array_to_pointer(Node *array_node) {
   Node *ref_to_array = new_node(ND_ADDR);
-  Type *ty_addr = calloc(1, sizeof(Type));
-  ty_addr->kind = TYPE_PTR;
+  Type *ty_addr = new_type(TYPE_PTR);
   ty_addr->ptr_to = array_node->type->ptr_to; // 配列の型はptr_toに入っている
   ty_addr->array_size = array_node->type->array_size;
   return new_typed_binary(new_typed_node(ty_addr, ref_to_array), array_node, NULL);
@@ -68,10 +80,8 @@ Node *add_type_to_node(Lvar *lvar_list, Node *node) {
       }
       return typed_stmt_head.next;
     }
-    case ND_NUM: { // ひとまずint型にする(もしポインタとの和の場合は case ND_ADD: など、後で修正される)
-      Type *ty = calloc(1, sizeof(Type));
-      ty->kind = TYPE_INT;
-      return new_typed_node(ty, node);
+    case ND_NUM: {
+      return new_typed_node(new_type(TYPE_INT), node);
     }
     case ND_LVARDEF:
     case ND_LVAR: {
@@ -102,9 +112,7 @@ Node *add_type_to_node(Lvar *lvar_list, Node *node) {
       if (lhs->type->kind != rhs->type->kind) {
         error("異なる型の変数を代入することはできません");
       }
-      Type *ty = calloc(1, sizeof(Type));
-      ty->kind = lhs->type->kind;
-      return new_typed_binary(new_typed_node(ty, node), lhs, rhs);
+      return new_typed_binary(new_typed_node(new_type(lhs->type->kind), node), lhs, rhs);
     }
     case ND_DEREF: {
       Node *lhs = add_type_to_node(lvar_list, node->lhs);
@@ -118,15 +126,12 @@ Node *add_type_to_node(Lvar *lvar_list, Node *node) {
     }
     case ND_ADDR: {
       Node *lhs = add_type_to_node(lvar_list, node->lhs);
-      Type *ty = calloc(1, sizeof(Type));
-      ty->kind = TYPE_PTR;
+      Type *ty = new_type(TYPE_PTR);
       ty->ptr_to = lhs->type;
       return new_typed_binary(new_typed_node(ty, node), lhs, NULL);
     }
     case ND_SIZEOF: {
       Node *lhs = add_type_to_node(lvar_list, node->lhs);
-      Type *ty = calloc(1, sizeof(Type));
-      ty->kind = TYPE_INT;
       Node *size_node = new_node(ND_NUM);
       if (lhs->type->kind == TYPE_INT) {
         size_node->val = SIZE_INT;
@@ -140,18 +145,14 @@ Node *add_type_to_node(Lvar *lvar_list, Node *node) {
         else
           size_node->val = SIZE_PTR * lhs->type->array_size;
       }
-      return new_typed_node(ty, size_node);
+      return new_typed_node(new_type(TYPE_INT), size_node);
     }
     case ND_RETURN: {
       Node *lhs = add_type_to_node(lvar_list, node->lhs);
-      Type *ty = calloc(1, sizeof(Type));
-      ty->kind = lhs->type->kind;
-      return new_typed_binary(new_typed_node(ty, node), lhs, NULL);
+      return new_typed_binary(new_typed_node(new_type(lhs->type->kind), node), lhs, NULL);
     }
     case ND_IF: {
-      Type *ty = calloc(1, sizeof(Type));
-      ty->kind = TYPE_NULL;
-      Node *typed_node = new_typed_node(ty, node);
+      Node *typed_node = new_typed_node(new_type(TYPE_NULL), node);
       Node *cond = add_type_to_node(lvar_list, node->cond);
       Node *then = add_type_to_node(lvar_list, node->then);
       if (node->els) {
@@ -163,9 +164,7 @@ Node *add_type_to_node(Lvar *lvar_list, Node *node) {
       return typed_node;
     }
     case ND_FOR: {
-      Type *ty = calloc(1, sizeof(Type));
-      ty->kind = TYPE_NULL;
-      Node *typed_node = new_typed_node(ty, node);
+      Node *typed_node = new_typed_node(new_type(TYPE_NULL), node);
       if (node->init) {
         Node *init = add_type_to_node(lvar_list, node->init);
         typed_node->init = init;
@@ -214,13 +213,7 @@ Node *add_type_to_node(Lvar *lvar_list, Node *node) {
       }
       // 左辺がポインタ型、右辺がint型の場合
       if (lhs->type->kind == TYPE_PTR && rhs->type->kind == TYPE_INT) {
-        Node *size = new_typed_node(new_type(TYPE_INT), new_node(ND_NUM));
-        if (lhs->type->ptr_to->kind == TYPE_INT) {
-          size->val = SIZE_INT;
-        }
-        else {
-          size->val = SIZE_PTR;
-        }
+        Node *size = new_size_node(lhs->type->ptr_to->kind);
         Node *mul_scaling = new_typed_binary(new_typed_node(new_type(TYPE_INT), new_node(ND_MUL)), size, rhs);
         return new_typed_binary(new_typed_node(lhs->type, node), lhs, mul_scaling);
       }
@@ -247,13 +240,7 @@ Node *add_type_to_node(Lvar *lvar_list, Node *node) {
 
       // 左辺がポインタ型、右辺がint型の場合
       if (lhs->type->kind == TYPE_PTR && rhs->type->kind == TYPE_INT) {
-        Node *size = new_typed_node(new_type(TYPE_INT), new_node(ND_NUM));
-        if (lhs->type->ptr_to->kind == TYPE_INT) {
-          size->val = SIZE_INT;
-        }
-        else {
-          size->val = SIZE_PTR;
-        }
+        Node *size = new_size_node(lhs->type->ptr_to->kind);
         Node *mul_scaling = new_typed_binary(new_typed_node(new_type(TYPE_INT), new_node(ND_MUL)), size, rhs);
         return new_typed_binary(new_typed_node(lhs->type, node), lhs, mul_scaling);
       }
@@ -261,13 +248,7 @@ Node *add_type_to_node(Lvar *lvar_list, Node *node) {
       else if (lhs->type->kind == TYPE_PTR && rhs->type->kind == TYPE_PTR) {
         if (lhs->type->ptr_to->kind != rhs->type->ptr_to->kind)
           error("異なる型へのポインタ同士で減算を行うことはできません");
-        Node *size = new_typed_node(new_type(TYPE_INT), new_node(ND_NUM));
-        if (lhs->type->ptr_to->kind == TYPE_INT) {
-          size->val = SIZE_INT;
-        }
-        else {
-          size->val = SIZE_PTR;
-        }
+        Node *size = new_size_node(lhs->type->ptr_to->kind);
         Node *diff_addr = new_typed_binary(new_typed_node(lhs->type, new_node(ND_SUB)), lhs, rhs);
         return new_typed_binary(new_typed_node(new_type(TYPE_INT), new_node(ND_DIV)), diff_addr, size);
       }
@@ -283,13 +264,9 @@ Node *add_type_to_node(Lvar *lvar_list, Node *node) {
     case ND_DIV: {
       Node *lhs = add_type_to_node(lvar_list, node->lhs);
       Node *rhs = add_type_to_node(lvar_list, node->rhs);
-      if (lhs->type->kind != rhs->type->kind) {
+      if (lhs->type->kind != rhs->type->kind)
         error("ポインタに対し乗法または除法を行うことはできません");
-      }
-      // そうでなければint同士の和差のはず
-      Type *ty = calloc(1, sizeof(Type));
-      ty->kind = TYPE_INT;
-      return new_typed_binary(new_typed_node(ty, node), lhs, rhs);
+      return new_typed_binary(new_typed_node(new_type(TYPE_INT), node), lhs, rhs);
     }
     case ND_EQ:
     case ND_NE:
@@ -297,18 +274,12 @@ Node *add_type_to_node(Lvar *lvar_list, Node *node) {
     case ND_LE: {
       Node *lhs = add_type_to_node(lvar_list, node->lhs);
       Node *rhs = add_type_to_node(lvar_list, node->rhs);
-      if (lhs->type->kind != rhs->type->kind) {
+      if (lhs->type->kind != rhs->type->kind)
         error("異なる型に対して比較を行うことはできません");
-      }
-      Type *ty = calloc(1, sizeof(Type));
-      ty->kind = lhs->type->kind;
-      return new_typed_binary(new_typed_node(ty, node), lhs, rhs);
+      return new_typed_binary(new_typed_node(new_type(lhs->type->kind), node), lhs, rhs);
     }
     default:
-      error(
-          "***************************\n"
-          "* INTERNAL COMPILER ERROR *\n"
-          "***************************\n");
+      error("nodeに型を付与することができませんでした");
   }
 }
 
