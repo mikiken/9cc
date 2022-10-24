0bb2ed2a11e1ef4112c7767646eba8edd48660da
Kentaro Miki <mikiken.dev@gmail.com>
2022-10-24 13:42:03 +0900

    Minor change in `test_driver.sh`

--- test/test_driver.sh
+++ test/test_driver.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-cat << EOT >> tmp.c
-#include <stdlib.h>
-#include <stdio.h>
-EOT
-cc -c tmp.c
-
-for file in test/*.c
+cd `dirname $0`
+for file in *.c
 do
-  ./9cc ${file%.*}.c > ${file%.*}.s
+  ../9cc ${file%.*}.c > ${file%.*}.s
   cc -c ${file%.*}.s
 done
 cc -o tmp *.o
